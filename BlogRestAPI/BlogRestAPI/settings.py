5b29f94572839d3b39c8b889a9faca4c9d384b4e
ahmetawt <ahmetsahiniu@gmail.com>
2021-09-14 16:41:03 +0300

    update

--- BlogRestAPI/BlogRestAPI/settings.py
+++ BlogRestAPI/BlogRestAPI/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +34,17 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ],
+    'DEFAULT_THROTTLE_CLASSES': {
+        'rest_framework.throttling.ScopedRateThrottle',
+    },
+    'DEFAULT_THROTTLE_RATES': {
+        'registerthrotte': '5/hour',
+        'ahmet':'5/hour',
+    }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15)
 }
 
 # Application definition
--- BlogRestAPI/account/urls.py
+++ BlogRestAPI/account/urls.py
@@ -3,4 +3,7 @@ from account import views
 
 urlpatterns = [
     path('me/', views.ProfileView.as_view(), name='myself'),
+    path('change-pw/', views.UpdatePassword.as_view(), name='change-password'),
+    path('register/', views.CreateUserView.as_view(), name='register'),
 ]
+
